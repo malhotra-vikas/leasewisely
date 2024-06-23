2a85d04bf1a6a98931db6857aced85eef3d81c90
Vikas Malhotra <malhotra.vikas@gmail.com>
2024-06-23 18:34:21 -0400

    Conversations

--- src/LeaseConversations/app.py
+++ src/LeaseConversations/app.py
@@ -1,12 +1,48 @@
 from flask import Flask, request, jsonify
+import api.leaseAPI as leaseAPI
 
 app = Flask(__name__)
 
-@app.route('/api/run-script', methods=['GET'])
+@app.route('/api/conversations', methods=['GET'])
 def run_script():
-    # Example: Running a simple Python script
-    result = {"message": "Script executed successfully!", "status": "success"}
-    return jsonify(result)
+
+    # Extract query parameters
+    email = request.args.get('email', type=str)
+    leaseUuid = request.args.get('leaseUuid', type=int)
+    userQuery = request.args.get('query', type=int)
+
+    # Check if parameters are not null
+    if email is None or leaseUuid is None or userQuery is None:
+        return jsonify({
+            "message": "Missing query parameters.",
+            "status": "error"
+        }), 400
+
+    message = f"Email is '{email}', Lease ID is '{leaseUuid}'."
+    print(message)
+
+    try:
+        # Call the leaseAPI and update the response accordingly
+        api_response = leaseAPI.handleConversation(email, leaseUuid, userQuery)
+        print("api_response is ", api_response)
+
+        if api_response["statusCode"] == 200:
+            return jsonify(api_response["body"]), 200
+        else:
+            return jsonify(api_response["body"]), api_response["statusCode"]
+
+    except Exception as error:
+        print("Error processing request:", error)
+        return jsonify({
+            "message": "Internal server error.",
+            "status": "error"
+        }), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+
+def buildConversation():
+    result = {"message": "Script executed successfully!", "status": "success"}
+    return jsonify(result)
