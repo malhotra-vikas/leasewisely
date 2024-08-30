3891a8d7bd99333f611d8e50f1f10256e6925c0e
Vikas Malhotra <malhotra.vikas@gmail.com>
2024-08-30 13:18:56 +0530

    Keep Alive Script

---
+++ src/PDF-To-Text/keep_running.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Infinite loop to keep the script running
+while true; do
+    # Run your Python script with nohup
+    nohup python3 parsePDFUtils.py > output.log 2>&1
+
+    # If the script crashes, wait for 5 seconds before restarting it
+    echo "The script has crashed! Restarting in 5 seconds..."
+    sleep 5
+done
